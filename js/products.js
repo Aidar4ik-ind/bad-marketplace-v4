69eb36d8ac1870431d650e0b39bd8690bd4e31ae
Aidar4ik-ind <99824658+Aidar4ik-ind@users.noreply.github.com>
2025-12-21 20:22:51 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -7,7 +7,7 @@ const productsData = [
         price: 1290,
         tags: ["магний", "стресс", "сон", "нервы"],
         category: "stress",
-        image: "https://unlimbot.hb.ru-msk.vkcloud-storage.ru/image/1/1d0d91df-6f78-4aa2-8cb9-57251fd3fa4d-e0c15c044b2f4d48af9253c5e15e3a78.png"
+        image: "b6ExtraPure.png"
     },
     {
         id: 2,
